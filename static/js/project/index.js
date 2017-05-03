ecccb35399a9b51a0a2deb45c5153a39b47aba83
simonzg <simonzg@gmail.com>
2017-05-03 16:49:17 -0700

    added moment and calendar

--- static/js/project/index.js
+++ static/js/project/index.js
@@ -3,6 +3,8 @@ $(function(){
     var pid = $(this).data('project_id');
     // prepare value for modal
     $('#project-id-input').val(pid);
+    var url = $(this).data('img_src')
+    $('#change-logo-current').attr('src', url);
     $('#change-logo-modal').modal('show');
   });
 
