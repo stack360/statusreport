bfaf3668a3dc03150ff53fb1efcccd5bbf4d7406
simonzg <simonzg@gmail.com>
2017-05-07 12:23:47 -0700

    meeting create

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,55 @@
-$(function(){
+/*
+     select-all / select-none Plugin for Select2
+*/
+$.fn.select2.amd.define('select2/selectAllAdapter', [
+    'select2/utils',
+    'select2/dropdown',
+    'select2/dropdown/attachBody'
+], function (Utils, Dropdown, AttachBody) {
+
+    function SelectAll() { }
+    SelectAll.prototype.render = function (decorated) {
+        var self = this,
+            $rendered = decorated.call(this),
+            $selectAll = $(
+                '<button class="btn btn-sm btn-secondary" type="button" style="margin-left:6px;"><i class="fa fa-check-square-o"></i> Select All</button>'
+            ),
+            $unselectAll = $(
+                '<button class="btn btn-sm btn-secondary" type="button" style="margin-left:6px;"><i class="fa fa-square-o"></i> Unselect All</button>'
+            ),
+            $btnContainer = $('<div style="margin-top:5px;margin-bottom:8px;">').append($selectAll).append($unselectAll);
+        if (!this.$element.prop("multiple")) {
+            // this isn't a multi-select -> don't add the buttons!
+            return $rendered;
+        }
+        $rendered.find('.select2-dropdown').prepend($btnContainer);
+        $selectAll.on('click', function (e) {
+            var $results = $rendered.find('.select2-results__option[aria-selected=false]');
+            $results.each(function () {
+                self.trigger('select', {
+                    data: $(this).data('data')
+                });
+            });
+            self.trigger('close');
+        });
+        $unselectAll.on('click', function (e) {
+            var $results = $rendered.find('.select2-results__option[aria-selected=true]');
+            $results.each(function () {
+                self.trigger('unselect', {
+                    data: $(this).data('data')
+                });
+            });
+            self.trigger('close');
+        });
+        return $rendered;
+    };
+
+    return Utils.Decorate(
+        Utils.Decorate(
+            Dropdown,
+            AttachBody
+        ),
+        SelectAll
+    );
+
 });
--- static/js/meeting/calendar.js
+++ static/js/meeting/calendar.js
@@ -1,9 +1,36 @@
 $(function(){
+  // initialize modal for meeting creation
+  $('#create-meeting-btn').click(function(){
+    $('#meeting-modal').modal('show');
+
+  });
+
+  // initialize date picker for date input
+  $('#date-input').datepicker({
+    autoclose: true,
+    todayHighlight: true,
+    startDate: new Date()
+  });
+
+  // initialize time picker
+  $('#time-input').clockpicker({
+    autoclose: true
+  });
+
+  // initialize select2
+  $('#user-select').select2({
+    theme: 'bootstrap',
+    width: '100%',
+    closeOnSelect: false,
+    dropdownAdapter: $.fn.select2.amd.require('select2/selectAllAdapter')
+  });
+
+  // initialize full calendar
   $('#calendar').fullCalendar({
     header: {
-      left: 'prev,next',
+      left: 'month,agendaWeek,agendaDay,listWeek',
       center: 'title',
-      right: 'month,agendaWeek,agendaDay,listWeek'
+      right: 'prev,next'
     },
     navLinks: true, // can click day/week names to navigate views
     editable: true,
@@ -16,7 +43,12 @@ $(function(){
       console.log(jsEvent);
       console.log(view.name);
     }
-
   });
+
+  // bind form save event
+  $('#meeting-save-btn').click(function(){
+    $('#user-input').val( $('#user-select').val().join(',') )
+    $('#meeting-form').submit();
+  })
 
 })
