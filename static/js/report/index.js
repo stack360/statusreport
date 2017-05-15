6bc4feb67d2a9112a7f132bb06a102925c2f65fc
simonzg <simonzg@gmail.com>
2017-05-15 12:37:19 -0700

    changes: 1. added project specific report link to project index 2. added project filter to report index page 3. memorize filter choice on report index page

--- static/js/report/index.js
+++ static/js/report/index.js
@@ -5,4 +5,20 @@ $(function(){
     itemSelector: '.card',
     gutter: 20
   });
+
+
+  $('.filter').change(function(){
+    var user_filter = $('#user-filter').val();
+    var project_filter = $('#project-filter').val();
+    var time_filter = $('#time-filter').val();
+    var params = [
+      project_filter ? 'project='+project_filter : '',
+      user_filter ? 'user='+user_filter : '',
+      time_filter ? 'time='+time_filter : ''
+    ]
+    params = params.filter(Boolean);
+    var querystr = '?'+params.join('&');
+    window.location.href= window.location.pathname + querystr;
+
+  })
 })
